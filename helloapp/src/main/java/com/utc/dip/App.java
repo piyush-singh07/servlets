077923fc6518c8a610bf7e6ee5f1e01cb2b0ce87
Piyush Singh <Piyush.Singh@utc.com>
2020-10-20 23:21:19 +0530

    app added

---
+++ helloapp/src/main/java/com/utc/dip/App.java
@@ -0,0 +1,13 @@
+package com.utc.dip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello source code" );
+    }
+}
